00400498395ebd9f45fcc730a6b5f60542684724
GomesRita <gomesrita541@gmail.com>
2026-08-09 17:00:43 -0300

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,14 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/..');
 }
 
+
 if (!class_exists('WP_REST_Controller')) {
-    abstract class WP_REST_Controller {}
+    class WP_REST_Controller {
+        protected $namespace = '';
+        protected $rest_base = '';
+    }
 }
+
 
 if (!class_exists('WP_REST_Response')) {
     class WP_REST_Response {}
